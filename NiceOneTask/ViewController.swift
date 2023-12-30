39962d7106add902bd26850f8159f3b8a5a4e5b5
Chaudhry Umair <chaudhryumair007@yahoo.com>
2023-12-30 18:45:15 +0500

    Initial Commit

---
+++ NiceOneTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiceOneTask
+//
+//  Created by Chaudhry Umair on 30/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
